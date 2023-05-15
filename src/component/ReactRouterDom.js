3a548bf8747e596fbd82d1372204d87f9230df6d
chan.yong.lee <chan.yong.lee@rwth-aachen.de>
2023-05-15 22:06:27 +0900

    initial commit

---
+++ src/component/ReactRouterDom.js
@@ -0,0 +1,10 @@
+import { useNavigate, useParams } from "react-router-dom"
+
+const ReactRouterDom = () => {
+    const {id} = useParams();
+    const navigate = useNavigate();
+
+    return <div></div>
+}
+
+export default ReactRouterDom

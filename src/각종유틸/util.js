459782b1bbd7df08db500a931f8e82b4cea56e3b
chan.yong.lee <chan.yong.lee@rwth-aachen.de>
2023-05-15 23:16:15 +0900

    update

--- src/각종유틸/util.js
+++ src/각종유틸/util.js
@@ -57,7 +57,7 @@ const SampleComponent2 = () => {
 // overflow 설정하는법
 /**
  * {
- *  overflowY: "scroll",
+ *  overflowY: "scroll", //auto가 나을듯
  *  overflowX: "hidden",
  *  height: "500px"     <-- 얘가 꼭 필요!!
  *

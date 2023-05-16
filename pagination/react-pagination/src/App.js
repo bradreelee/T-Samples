48fad192ae7f7e30e74435e812e9372cce837261
chan.yong.lee <chan.yong.lee@rwth-aachen.de>
2023-05-16 21:22:46 +0900

    highlight

--- pagination/react-pagination/src/App.js
+++ pagination/react-pagination/src/App.js
@@ -6,10 +6,13 @@ import "./style.scss";
 // code completely from https://github.com/mayankshubham/react-pagination
 // currently
 
-// 한 페이지에 표시되는 아이템의 갯수 제거
+// 한 페이지에 표시되는 아이템의 갯수
+//////////////////////////////////////////////////////////////////////////
 let PageSize = 10;
+//////////////////////////////////////////////////////////////////////////
 
 export default function App() {
+  //////////////////////////////////////////////////////////////////////////
   const [currentPage, setCurrentPage] = useState(1);
   // 화면에 뿌릴 데이터
   const [data, setData] = useState(sampleData.slice(0, 100));
@@ -19,6 +22,7 @@ export default function App() {
     const lastPageIndex = firstPageIndex + PageSize;
     return data.slice(firstPageIndex, lastPageIndex);
   }, [currentPage, data]);
+  //////////////////////////////////////////////////////////////////////////
 
   return (
     <div>
@@ -33,6 +37,7 @@ export default function App() {
           </tr>
         </thead>
         <tbody>
+          {/* ///////////////////////////////////// */}
           {currentTableData.map((item) => {
             return (
               <tr>
@@ -44,9 +49,10 @@ export default function App() {
               </tr>
             );
           })}
+          {/* ///////////////////////////////////// */}
         </tbody>
       </table>
-      {currentTableData && currentTableData.map((item) => <div>{item.id}</div>)}
+      {/* ///////////////////////////////////// */}
       <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
         <Pagination
           currentPage={currentPage}
@@ -55,6 +61,7 @@ export default function App() {
           onPageChange={(page) => setCurrentPage(page)}
         />
       </div>
+      {/* ///////////////////////////////////// */}
     </div>
   );
 }
